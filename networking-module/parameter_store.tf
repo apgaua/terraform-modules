a3870c644c236aa80ea7b980b27eee199bd1b3fc
apgaua <apgaua.sousa@gmail.com>
2025-01-08 20:36:49 -0300

    Update parameter store

--- networking-module/parameter_store.tf
+++ networking-module/parameter_store.tf
@@ -3,3 +3,35 @@ resource "aws_ssm_parameter" "vpc" {
   type  = "String"
   value = aws_vpc.main.id
 }
+
+
+resource "aws_ssm_parameter" "publicsubnets" {
+  count = length(aws_subnet.public)
+  name  = "/${var.project_name}/subnets/public/${var.publicsubnets[count.index].availability_zone}/${var.publicsubnets[count.index].name}"
+  type  = "String"
+  value = aws_subnet.publicsubnets[count.index].id
+  tags = {
+      Name = "/${var.project_name}/subnets/public/${var.publicsubnets[count.index].availability_zone}/${var.publicsubnets[count.index].name}"
+    }
+}
+
+resource "aws_ssm_parameter" "privatesubnets" {
+  count = length(aws_subnet.private)
+
+  name  = "/${var.project_name}/subnets/private/${var.privatesubnets[count.index].availability_zone}/${var.privatesubnets[count.index].name}"
+  type  = "String"
+  value = aws_subnet.privatesubnets[count.index].id
+  tags = {
+      Name = "/${var.project_name}/subnets/private/${var.privatesubnets[count.index].availability_zone}/${var.privatesubnets[count.index].name}"
+    }
+}
+
+resource "aws_ssm_parameter" "databasesubnets" {
+  count = length(aws_subnet.database)
+  name  = "/${var.project_name}/subnets/databases/${var.databasesubnets[count.index].availability_zone}/${var.databasesubnets[count.index].name}"
+  type  = "String"
+  value = aws_subnet.dbsubnets[count.index].id
+  tags = {
+      Name = "/${var.project_name}/subnets/databases/${var.databasesubnets[count.index].availability_zone}/${var.databasesubnets[count.index].name}"
+}
+}
