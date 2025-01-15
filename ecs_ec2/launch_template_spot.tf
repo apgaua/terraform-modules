13c492543929c4b583707efc7d332e66ade9e1c4
apgaua <apgaua.sousa@gmail.com>
2025-01-14 21:13:47 -0300

    Organizing launch template files

--- ecs_ec2/launch_template_spot.tf
+++ ecs_ec2/launch_template_spot.tf
@@ -1,36 +1,3 @@
-## ON DEMAND Launch Template
-resource "aws_launch_template" "on_demand" {
-  name_prefix            = format("%s-on-demand", var.project_name)
-  image_id               = var.nodes_ami
-  instance_type          = var.node_instance_type
-  vpc_security_group_ids = [aws_security_group.main.id]
-
-  iam_instance_profile {
-    name = aws_iam_instance_profile.iam_role.name
-  }
-  update_default_version = true
-
-  block_device_mappings {
-    device_name = "/dev/xvda"
-    ebs {
-      volume_size = var.node_volume_size
-      volume_type = var.node_volume_type
-    }
-  }
-
-  tag_specifications {
-    resource_type = "instance"
-    tags = {
-      Name = format("%s-on-demand", var.project_name)
-    }
-  }
-
-  user_data = base64encode(templatefile("${path.module}/templates/user-data.tpl", {
-    CLUSTER_NAME = var.project_name
-  }))
-}
-
-
 ## SPOT Launch Template
 resource "aws_launch_template" "spot" {
   name_prefix            = format("%s-spot", var.project_name)
--- ecs_ec2/variables.tf
+++ ecs_ec2/variables.tf
@@ -11,7 +11,6 @@ variable "ssm_vpc_id" {
     type = string
     default = "/ECS/vpc/id"
 }
-
 variable "privatesubnets" {
     type = list(string)
 }
@@ -24,55 +23,42 @@ variable "databasesubnets" {
 ##############################################################
 ################### LOAD BALANCER VARIABLES ##################
 ##############################################################
-
 variable "load_balancer_internal" {
   type = bool
 }
-
 variable "load_balancer_type" {
   type = string
 }
-
 ##############################################################
 ####################### ECS VARIABLES ########################
 ##############################################################
-
 variable "nodes_ami" {
   type = string
 }
-
 variable "node_instance_type" {
   type = string
 }
-
 variable "node_volume_size" {
   type = number
 }
-
 variable "node_volume_type" {
   type = string
 }
-
 variable "cluster_ondemand_min" {
   type = number
 }
-
 variable "cluster_ondemand_max" {
   type = number
 }
-
 variable "cluster_ondemand_desired" {
   type = number
 }
-
 variable "cluster_spot_min" {
   type = number
 }
-
 variable "cluster_spot_max" {
   type = number
 }
-
 variable "cluster_spot_desired" {
   type = number
 }
