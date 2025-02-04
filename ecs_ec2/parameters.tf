b0663fcef2392cef3da25cc9ddc335adc952e9f9
apgaua <apgaua.sousa@gmail.com>
2025-02-04 20:59:31 -0300

    Fix ssm parameter value

--- ecs_ec2/parameters.tf
+++ ecs_ec2/parameters.tf
@@ -1,11 +1,11 @@
 resource "aws_ssm_parameter" "lb_arn" {
-  name  = "/ECS/loadbalancer/id"
+  name  = "/${var.project_name}/loadbalancer/id"
   value = aws_lb.main.arn
   type  = "String"
 }
 
 resource "aws_ssm_parameter" "lb_listener" {
-  name  = "/ECS/loadbalancer/listener"
+  name  = "/${var.project_name}/loadbalancer/listener"
   value = aws_lb_listener.main.arn
   type  = "String"
 }
