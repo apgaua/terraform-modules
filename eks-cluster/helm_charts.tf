d856f8f38c7de61eff51d67640a719fc03b2e3f3
apgaua <apgaua.sousa@gmail.com>
2025-09-05 18:01:24 -0300

    Set termination handler

--- eks-cluster/helm_charts.tf
+++ eks-cluster/helm_charts.tf
@@ -50,3 +50,54 @@ resource "helm_release" "cluster_autoscaler" {
 
   depends_on = [aws_eks_cluster.main, aws_eks_node_group.main]
 }
+
+resource "helm_release" "node_termination_handler" {
+  name      = "aws-node-termination-handler"
+  namespace = "kube-system"
+
+  chart      = "aws-node-termination-handler"
+  repository = "https://aws.github.io/eks-charts/"
+
+  set = [
+    {
+    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = aws_iam_role.node_termination.arn
+  },
+  {
+    name  = "awsRegion"
+    value = data.aws_region.current.id
+  },
+  {
+    name  = "queueURL"
+    value = aws_sqs_queue.node_termination.url
+  },
+  {
+    name  = "enableSqsTerminationDraining"
+    value = true
+  },
+  {
+    name  = "enableSpotInterruptionDraining"
+    value = true
+  },
+  {
+    name  = "enableRebalanceMonitoring"
+    value = true
+  },
+  {
+    name  = "enableRebalanceDraining"
+    value = true
+  },
+  {
+    name  = "enableScheduledEventDraining"
+    value = true
+  },
+  {
+    name  = "deleteSqsMsgIfNodeNotFound"
+    value = true
+  },
+  {
+    name  = "checkTagBeforeDraining"
+    value = false
+  }
+  ]
+}
