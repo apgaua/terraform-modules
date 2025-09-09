5ecea0d4ea66be9a8a439f9ab677f4405c6e1fc4
apgaua <apgaua.sousa@gmail.com>
2025-09-09 17:49:28 -0300

    Configure module to allow fargate profiles deployment

--- eks-cluster/access_entries.tf
+++ eks-cluster/access_entries.tf
@@ -3,3 +3,10 @@ resource "aws_eks_access_entry" "nodes" {
   principal_arn = aws_iam_role.eks_nodes_role.arn
   type          = "EC2_LINUX"
 }
+
+resource "aws_eks_access_entry" "fargate" {
+  count = length(var.cluster) > 0 && var.cluster[0].enable_fargate ? 1 : 0
+  cluster_name  = aws_eks_cluster.main.id
+  principal_arn = aws_iam_role.fargate_cluster_role[0].arn
+  type          = "FARGATE_LINUX"
+}
