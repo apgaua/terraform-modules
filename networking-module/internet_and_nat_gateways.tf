5a378c1ae3fccaa9bcaafcfa17bc14a8cbf21275
apgaua <apgaua.sousa@gmail.com>
2025-09-18 23:30:28 -0300

    Terraform fmt

--- networking-module/internet_and_nat_gateways.tf
+++ networking-module/internet_and_nat_gateways.tf
@@ -5,7 +5,7 @@
 resource "aws_eip" "eip" {
   count  = var.singlenat == true ? 1 : length(var.publicsubnets)
   domain = "vpc"
-  tags = merge({ Name = format("eip-%s", var.project_name) }, var.default_tags)
+  tags   = merge({ Name = format("eip-%s", var.project_name) }, var.default_tags)
 }
 
 ##################################################
@@ -14,7 +14,7 @@ resource "aws_eip" "eip" {
 
 resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.main.id
-  tags = merge({ Name = format("%s-igw", var.project_name) }, var.default_tags)
+  tags   = merge({ Name = format("%s-igw", var.project_name) }, var.default_tags)
 }
 
 ##################################################
@@ -22,9 +22,9 @@ resource "aws_internet_gateway" "gw" {
 ##################################################
 
 resource "aws_nat_gateway" "main" {
-  count        = var.nat_gateway_type == "GATEWAY" ? (var.singlenat == true ? 1 : length(var.publicsubnets)) : 0
+  count         = var.nat_gateway_type == "GATEWAY" ? (var.singlenat == true ? 1 : length(var.publicsubnets)) : 0
   allocation_id = aws_eip.eip[count.index].id
   subnet_id     = aws_subnet.publicsubnets[count.index].id
-  tags       = merge({ Name = format("%s-nat-gateway-%s", var.project_name, count.index) }, var.default_tags)
-  depends_on = [aws_internet_gateway.gw, aws_eip.eip, aws_subnet.publicsubnets]
+  tags          = merge({ Name = format("%s-nat-gateway-%s", var.project_name, count.index) }, var.default_tags)
+  depends_on    = [aws_internet_gateway.gw, aws_eip.eip, aws_subnet.publicsubnets]
 }
